b2ec043465b956c9ea1d936a2ffe2d45a1497390
Andrey999K <andrey1234xxxx@mail.ru>
2020-07-06 15:17:15 +0300

    06.07.2020

---
+++ 1.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", () => {
+
+//     let request = new XMLHttpRequest();
+//     request.open('GET', 'https://andrey999k.github.io/Portfolio-level1/build/');
+//     request.onreadystatechange = function () {
+
+//         if ((request.readyState === 4) &&
+//             (request.status = 200)) {
+//             let words;
+//             words = request.response;
+//             console.log(words);
+//             document.querySelector("body").innerHTML = words;
+//         }
+//     }
+//     request.send();
+// });
